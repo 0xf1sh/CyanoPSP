957239ab6769fcc14defaa3f50aebc607f249114
joel16 <sauskethunderkid@gmail.com>
2014-04-23 21:07:50 -0500

    Added home menu
    Using the directional buttons, the cursor can be moved. Home menu is now
    working.

--- main.c
+++ main.c
@@ -4,18 +4,16 @@
 #include <pspnet_apctl.h>
 #include <oslib/oslib.h>
  
-PSP_MODULE_INFO("OSLib Sample", 0, 1, 0);
+PSP_MODULE_INFO("CyanogenMod PSP", 0, 1, 0);
 PSP_MAIN_THREAD_ATTR(THREAD_ATTR_USER | THREAD_ATTR_VFPU);
 PSP_HEAP_SIZE_KB(4*1024);
- 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 // Globals:
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 int runningFlag = 1;
  
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Callbacks:
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 /* Exit callback */
 int exit_callback(int arg1, int arg2, void *common) {
     runningFlag = 0;
@@ -40,61 +38,79 @@ int SetupCallbacks(void) {
         sceKernelStartThread(thid, 0, 0);
     return thid;
 }
- 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Main:
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-int main(){
-    int skip = 0;
-    char message[100] = "";
-    SetupCallbacks();
- 
-    //Initialize OSLib
-    oslInit(0);
-    oslInitGfx(OSL_PF_8888, 1);
-    oslSetQuitOnLoadFailure(1);
-    oslSetKeyAutorepeatInit(40);
-    oslSetKeyAutorepeatInterval(10);
- 
-    //This is for the font system
-    oslIntraFontInit(INTRAFONT_CACHE_MED);
-	
-	    //Load font:
-    OSL_FONT *font = oslLoadFontFile("flash0:/font/ltn0.pgf");
-    oslSetFont(font);
- 
-//to load images
-OSL_IMAGE * boot[4];
-boot[0] = oslLoadImageFilePNG("System/Kernel/Boot/1.png", OSL_IN_RAM | OSL_SWIZZLED, OSL_PF_8888);
-boot[1] = oslLoadImageFilePNG("System/Kernel/Boot/2.png", OSL_IN_RAM | OSL_SWIZZLED, OSL_PF_8888);
-boot[2] = oslLoadImageFilePNG("System/Kernel/Boot/3.png", OSL_IN_RAM | OSL_SWIZZLED, OSL_PF_8888);
-boot[3] = oslLoadImageFilePNG("System/Kernel/Boot/4.png", OSL_IN_RAM | OSL_SWIZZLED, OSL_PF_8888);
 
-//to draw
-int current_frame = 0;
-while(runningFlag && !osl_quit)
+//declaration
+OSL_IMAGE *background, *cursor, *appicon, *appicon2, *navbar, *wificon, *apollo, *gmail, *message, *browser, *google;
+ 
+int main()
 {
-	oslStartDrawing();
-	oslDrawImageXY(boot[current_frame], 0, 0); 
-	
-	sceDisplayWaitVblankStart();
-	oslEndDrawing();
-	oslEndFrame();
-	skip = oslSyncFrame();
-	sceKernelDelayThread(1000000 * 1);
-	
-	current_frame++;
-	if(current_frame > 3)
-		current_frame = 0;
-    //Quit OSL:
-    
-    oslEndGfx();
-    oslQuit();
- 
-    sceKernelExitGame();
-    return 0;
- 
-}
-}
+	//Initialization of the Oslib's library
+	oslInit(0);
 
+	//Initialization of Oslib's graphics mode
+	oslInitGfx(OSL_PF_8888, 1);
+	
+	//Sets the transparency color (blue)
+	oslSetTransparentColor(RGB(0,0,255));
+
+	//loads our images into memory
+	background = oslLoadImageFile("System/Home/Wallpapers/1.png", OSL_IN_RAM, OSL_PF_5551);
+	cursor = oslLoadImageFilePNG("System/Cursor/cursor.png", OSL_IN_RAM, OSL_PF_5551);
+	appicon = oslLoadImageFilePNG("System/Home/Icons/appicon1.png", OSL_IN_RAM, OSL_PF_5551);
+	appicon2 = oslLoadImageFile("System/Home/Icons/appicon2.png", OSL_IN_RAM, OSL_PF_5551);
+	navbar = oslLoadImageFile("System/Home/Icons/nav.png", OSL_IN_RAM, OSL_PF_5551);
+	wificon = oslLoadImageFile("System/Home/Icons/wificon.png", OSL_IN_RAM, OSL_PF_5551);
+	apollo = oslLoadImageFilePNG("System/Home/Icons/apollo.png", OSL_IN_RAM, OSL_PF_5551);
+	gmail = oslLoadImageFilePNG("System/Home/Icons/gmail.png", OSL_IN_RAM, OSL_PF_5551);
+	message = oslLoadImageFilePNG("System/Home/Icons/message.png", OSL_IN_RAM, OSL_PF_5551);
+	browser = oslLoadImageFile("System/Home/Icons/browser.png", OSL_IN_RAM, OSL_PF_5551);
+	google = oslLoadImageFile("System/Home/Icons/google.png", OSL_IN_RAM, OSL_PF_5551);
+	
+	//Disables the transpaent color (blue)
+	oslDisableTransparentColor();
+	
+	//Debugger
+	if (!background || !cursor)
+		oslDebug("It seems certain files necessary for the program to run are missing. Please make sure you have all the files required to run the program.");
+
+	//Main loop to run the program
+	while (!osl_quit)
+	{
+		//Draws images onto the screen
+		oslStartDrawing();
+		
+		//Initiate the PSP's controls
+		oslReadKeys();
+		
+		//Cursor movement
+		if (osl_keys->held.down) cursor->y += 4;
+		if (osl_keys->held.up) cursor->y -= 4;
+		if (osl_keys->held.left) cursor->x -= 4;
+		if (osl_keys->held.right) cursor->x += 4;
+
+		//Print the images onto the screen
+		oslDrawImage(background);
+		oslDrawImage(cursor);
+		oslDrawImageXY(navbar, 103, 241);
+		oslDrawImageXY(wificon, 387, 1);
+		oslDrawImageXY(google, 22, 26);
+		oslDrawImageXY(apollo, 105, 195);
+		oslDrawImageXY(browser, 276, 195);
+		oslDrawImageXY(gmail, 331, 195);
+		oslDrawImageXY(message, 160, 195);
+		oslDrawImageXY(appicon2, 223, 205);
+		oslDrawImage(cursor);
+
+		//Ends printing
+		oslEndDrawing();
+
+		//Synchronizes the screen 
+		oslSyncFrame();		
+	}
+	
+	//Terminates/Ends the program
+	oslEndGfx();
+	oslQuit();
+	return 0;
+}
 
